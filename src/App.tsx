a4fe90fe4a74d497ffd6d0fba0b9cc5bb9cf5ddd
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-03-14 14:31:10 +0800

    feat: update

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+
+const App = (props: any) => {
+  useEffect(()=>{
+    console.log('123')
+  },)
+  return (
+    <div>
+      fghfghf
+    </div>
+  )
+}
+
+export default App
